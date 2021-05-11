ebd078679639e1a2c9359b074802e6f0a1b71bad
fconstancio <constanciosalomao@gmail.com>
2021-05-11 17:22:00 -0300

    adicionado teste ao gitignore e melhoria classe-pos

--- ProjetoESCOLA/src/classes/Pos.java
+++ ProjetoESCOLA/src/classes/Pos.java
@@ -5,7 +5,7 @@ public class Pos extends Estudante {
 
 	public Pos(int matricula, String cpf) {
 		super(matricula, cpf);
-		credito = 10f;
+		credito = 5f;
 	}
 
 	public double getCredito() {
@@ -18,10 +18,13 @@ public class Pos extends Estudante {
 
 	public void pedirCreditoPos(double credito) {
 		if (this.credito >= credito) {
+			System.out.printf("Você usou %.2f créditos.\n", (credito));
 			super.adicionarNota(credito);
+			this.credito -= credito;
+			System.out.printf("Você possui %.2f créditos.\n", this.credito);
 		} else {
-			System.out.println("Valor de créditos incorreto.");
-			System.out.printf("Você possui %.2f\n", this.credito);
+			System.out.println("Valor de créditos incorreto.\n");
+			System.out.printf("Você possui %.2f créditos.\n", this.credito);
 		}
 	}
 
@@ -32,7 +35,14 @@ public class Pos extends Estudante {
 
 	@Override
 	public void tirarNota(double pontos) {
-		super.tirarNota(pontos);
+		if(super.getPontos()>= pontos) {
+			super.tirarNota(pontos);
+		}else if ((super.getPontos() + getCredito())>= pontos) {
+			pedirCreditoPos((pontos - super.getPontos()));
+			super.tirarNota(pontos);
+		}else {
+			System.out.println("Você não tem pontos e creditos o suficiente.");
+		}
 	}
 
 }
--- ProjetoESCOLA/src/programas/ProgramaTeste.java
+++ ProjetoESCOLA/src/programas/ProgramaTeste.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 import classes.Basico;
 import classes.Estudante;
+import classes.Pos;
 
 public class ProgramaTeste {
 
@@ -11,26 +12,11 @@ public class ProgramaTeste {
 	{
 		Scanner leia = new Scanner (System.in);
 		
-		System.out.println("Digite o nome do aluno");
-		String nome = leia.next();
-		System.out.println("Informe a matricula");
-		int matricula = leia.nextInt();
-		System.out.println("Informe o CPF");
-		String cpf = leia.next();
-		System.out.println("dia do niver:");
-		int dia = leia.nextInt();
-		System.out.println("Nota inicial: ");
-		double nota = leia.nextDouble();
-		
-		Basico aluno1 = new Basico (matricula, cpf, dia); //instanciando o objeto
-		aluno1.setNome(nome);
-		aluno1.adicionarNota(10);
-		System.out.println("PONTOS ATUAIS: "+aluno1.getPontos());
-		System.out.println("Dia atual");
-		int diaAtual = leia.nextInt();
-		
-		aluno1.bonusAniversario(diaAtual);
-		System.out.println("PONTOS após metodo: "+aluno1.getPontos());
+		Pos aluno1 = new Pos(123,"111.222.333.444-11");
+		aluno1.adicionarNota(5);
+		System.out.printf("nota %.2f: \n", aluno1.getPontos());
+		aluno1.tirarNota(11);
+		System.out.printf("nota %.2f: ", aluno1.getPontos());
 	}
 
 }
