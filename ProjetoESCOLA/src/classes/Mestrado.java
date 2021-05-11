4a0280dea54f77a27ace9570ec947da37be445d7
Renato-Nakamura <renato.yukio30@hotmail.com>
2021-05-11 14:25:08 -0300

    teste mestrado

---
+++ ProjetoESCOLA/src/classes/Mestrado.java
@@ -0,0 +1,11 @@
+package classes;
+
+public class Mestrado extends Estudante{
+	int nota;
+
+	public Mestrado(int matricula, String cpf, int nota) {
+		super(matricula, cpf);
+		this.nota = nota;
+	}
+	
+}
